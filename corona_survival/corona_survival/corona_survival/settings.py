a73ec3b58123afacb55e89209f4f7d701cafd053
pclnwk <pascal-nowak@gmx.de>
2020-03-23 00:06:02 +0100

    add hint message

--- corona_survival/corona_survival/corona_survival/settings.py
+++ corona_survival/corona_survival/corona_survival/settings.py
@@ -76,10 +76,10 @@ WSGI_APPLICATION = 'corona_survival.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
+     #'default': {
+     #    'ENGINE': 'django.db.backends.sqlite3',
+     #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+     #}
     'default': {
       'ENGINE': 'django.db.backends.mysql',
       'NAME': 'web74_wirvsvirus_substitution',
