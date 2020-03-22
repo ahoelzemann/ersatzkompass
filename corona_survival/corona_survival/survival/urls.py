ef4782eec99b580dae975eb1aa527ebbefba294f
pclnwk <pascal-nowak@gmx.de>
2020-03-22 15:28:57 +0100

    update navigation

--- corona_survival/corona_survival/survival/urls.py
+++ corona_survival/corona_survival/survival/urls.py
@@ -6,11 +6,12 @@ from django.conf import settings
 
 urlpatterns = [
     path('', views.home, name='cs-home'),
-    path('items/', views.item, name='cs-items'),
+    path('items/', views.items, name='cs-items'),
+    path('items/sorted/<int:subcategory_id>', views.items_sorted, name='cs-items-sorted'),
     path('item/<int:pk>/', views.ItemDetailView.as_view(), name='item-detail'),
-    path('categories/', views.category, name='cs-categories'),
+    path('categories/', views.categories, name='cs-categories'),
     path('category/<int:pk>/', views.CategoryDetailView.as_view(), name='category-detail'),
-    path('substitutions/', views.substitution, name='cs-substitutions'),
+    path('substitutions/', views.substitutions, name='cs-substitutions'),
     path('login/', auth_views.LoginView.as_view(template_name='survival/login.html'), name='login'),
     path('admin2/', views.admin, name='cs-admin'),
     path('logout/', auth_views.LogoutView.as_view(template_name='survival/logout.html'), name='logout'),
--- corona_survival/corona_survival/survival/views.py
+++ corona_survival/corona_survival/survival/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.decorators import login_required
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from .models import Item, Category, Substitution
+from .models import Item, Category, Substitution, Comment, Subcategory
 from random import *
 
 # Create your views here.
@@ -11,36 +11,59 @@ def home(request):
     context = {
         'categories': Category.objects.all(),
         'items': items,
-        'title': 'ErsatzKompass'
     }
     return render(request, 'survival/home.html', context)
 
-def category(request):
+def categories(request):
     context = {
         'categories': Category.objects.all(),
-        'title':'Corona Survival - Categories'
     }
     return render(request, 'survival/categories.html', context)
+
+#class CommentCreateView(CreateView):
+#    model = Comment
+#    fields = ['name', 'text']
+#
+#    def get_context_data(self, **kwargs):
+#        context = super().get_context_data(**kwargs)
+#        context['categories'] = Substitution.objects.all()
+#        return context
 
 class ItemDetailView(DetailView):
     model = Item    
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        return context
+
 class CategoryDetailView(DetailView):
     model = Category
 
-def item(request):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        return context
+
+def items(request):
     context = {
         'items': Item.objects.all(),
         'categories': Category.objects.all(),
-        'title':'Items'
     }
     return render(request, 'survival/items.html', context)
 
-def substitution(request):
+def items_sorted(request, subcategory_id):
+    items = Item.objects.filter(subcategory_id=subcategory_id)
+    context = {
+        'items': items,
+        'categories': Category.objects.all(),
+    }
+    return render(request, 'survival/items.html', context)
+
+def substitutions(request):
     context = {
         'categories': Category.objects.all(),
         'substitutions': Substitution.objects.all(),
-        'title':'Substitutions'
     }
     return render(request, 'survival/substitutions.html', context)
 
@@ -51,6 +74,5 @@ def admin(request):
         'categories': Category.objects.all(),
         'subcategories': Item.objects.all(),
         'substitutions': Substitution.objects.all(),
-        'title':'Admin page'
     }
     return render(request, 'survival/admin.html', context)
