02275933feb8ce1025c1111fd4e6d59349986df3
Alexander Hölzemann <alexander.hoelzemann@student.uni-siegen.de>
2020-03-23 01:03:38 +0100

    production mode

--- corona_survival/corona_survival/survival/urls.py
+++ corona_survival/corona_survival/survival/urls.py
@@ -18,7 +18,7 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='survival/logout.html'), name='logout'),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-else:
-    urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# if settings.DEBUG:
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# else:
+#     urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
