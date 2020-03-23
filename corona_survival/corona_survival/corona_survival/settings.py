0faf01f7184013a0827446413aed55cf1ab84018
Alexander Hölzemann <alexander.hoelzemann@student.uni-siegen.de>
2020-03-23 01:03:29 +0100

    production mode

--- corona_survival/corona_survival/corona_survival/settings.py
+++ corona_survival/corona_survival/corona_survival/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'y@%49!af@@pen=khyv!$6*y2&ghg&3pwhe*gyi@jogcg@jkbob'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['corsubst.uber.space', '127.0.0.1']
 
